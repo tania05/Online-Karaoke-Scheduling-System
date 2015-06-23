7d36ee44ebb65fa21451902e332e095cc22bfee5
Justin Macaulay <jmac@c91.seng.uvic.ca>
2015-06-23 13:59:52 -0700

    modified the config.js

--- config.js
+++ config.js
@@ -3,17 +3,3 @@ module.exports = {
     'database': 'mongodb://purple:fox@ds036648.mongolab.com:36648/purple-fox-karaokedb',
     'secret': 'type something here'
 };
-
-var config  = require('./config');
-
-// connect to our database (hosted at ????).
-mongoose.connect(config.database);
-
-// START THE SERVER
-// ======================
-app.listen(config.port);
-console.log('Magic happens on port ' + config.port);
-
-// super secret for creating tokens.
-var superSectrect = 'type something secret here.';
-    
