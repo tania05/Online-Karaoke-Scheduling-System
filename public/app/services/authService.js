5d87b877182c769859da7a2bf1ff1a7325df82c1
Joshua Portelance <joshua_portelance@hotmail.com>
2015-06-26 12:38:26 -0700

    updated

--- public/app/services/authService.js
+++ public/app/services/authService.js
@@ -43,7 +43,7 @@
     // get the logged in user
     authFactory.getUser = function() {
         if(AuthToken.getToken())
-            return $http.get('/api/me');
+            return $http.get('/api/me', { cache: true });
         else
             return $q.reject({ message: 'User has no token.' });
     };
