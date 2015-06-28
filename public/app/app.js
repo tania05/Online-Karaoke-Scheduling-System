5e3d623e636024f6d1073f3bb2ffb69f6b55f958
Sam Taylor <sat@uvic.ca>
2015-06-28 13:33:41 -0700

    commit again and again

--- public/app/app.js
+++ public/app/app.js
@@ -6,12 +6,6 @@ angular.module('karaokeApp', ['app.routes', 'contactCtrl', 'bookingCtrl', 'userC
 /*
 	// attach our auth interceptor to the http requests
 	$httpProvider.interceptors.push('AuthInterceptor');
-
 	*/
 
 });
-
-
-
-
-
