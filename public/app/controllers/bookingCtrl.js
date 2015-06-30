797bb2e75bdfd7b07bffaa5e9b23526801dc9c8b
Tania Akter <tania9309@gmail.com>
2015-06-30 14:26:39 -0700

    added create  booking controller

--- public/app/controllers/bookingCtrl.js
+++ public/app/controllers/bookingCtrl.js
@@ -2,7 +2,7 @@
 
 angular.module('bookingCtrl', [bookingService])
 
-.controller('bookingCreateController', function() {
+.controller('bookingController', function() {
 
     var vm = this;
     vm.message = 'Injected Booking Page!';
@@ -14,7 +14,31 @@ angular.module('bookingCtrl', [bookingService])
 // ----
 // This controller handles the process of creating a booking
 // ====
+.controller('userCreateController', function(Booking){
+	
+	var vm = this;
+	
+	//variable to differenctialte edit and create bookings
+	vm.type='create';
+	
+	// function to crreate the booking
+	
+	vm.saveBooking = function(){
+	
+	vm.processing = true;
+	vm.message= '';
+	
+	// use the create function on bookingService
+	Booking.create(vm.bookingData)
+		.success(function(data){
+			vm.processing =false;
+			vm.bookingData={};
+			vm.message= data.message;
+		})
+	
+	}
 
+})
     // ====
     // functions...
     // ====
