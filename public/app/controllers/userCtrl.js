021798cf8e8278a3f4d843410f0789cacfa84b79
staylor95 <samt.95@live.ca>
2015-07-12 20:40:29 -0700

    register form validation

--- public/app/controllers/userCtrl.js
+++ public/app/controllers/userCtrl.js
@@ -67,32 +67,6 @@ angular.module('userCtrl', ['userService', 'ui.bootstrap.showErrors', 'authServi
 })
 
 
-.directive('showErrors', function() {
-    return {
-          restrict: 'A',
-          require: '^form',
-          link: function (scope, el, attrs, formCtrl) {
-            // find the text box element, which has the 'name' attribute
-            var inputEl   = el[0].querySelector("[name]");
-            // convert the native text box element to an angular element
-            var inputNgEl = angular.element(inputEl);
-            // get the name on the text box
-            var inputName = inputNgEl.attr('name');
-            
-            // only apply the has-error class after the user leaves the text box
-            inputNgEl.bind('blur', function() {
-              el.toggleClass('has-error', formCtrl[inputName].$invalid);
-            });
-            
-            scope.$watch(function() {
-              return scope.showErrorsCheckValidity;
-            }, function(newVal, oldVal) {
-              if (!newVal) { return; }
-              el.toggleClass('has-error', formCtrl[inputName].$invalid);
-            });
-          }
-        }
-})
 
 // ====
 // ----
@@ -109,6 +83,7 @@ angular.module('userCtrl', ['userService', 'ui.bootstrap.showErrors', 'authServi
      vm.type = 'create';
      vm.complete = false;
      vm.invalidUserName = false;
+     vm.noPassMatch = false;
 
     Auth.getUser()
         .then(function(data) {
@@ -122,13 +97,11 @@ angular.module('userCtrl', ['userService', 'ui.bootstrap.showErrors', 'authServi
         vm.processing = true;
         vm.message = '';
         vm.invalidUserName = false;
+        vm.noPassMatch = false;
 
         vm.showErrorsCheckValidity = true;
 
-        // if the input is not valid, do not save the user
-        // FOR SOME REASON $valid CAUSES ERRORS
-        // if(vm.userForm.$valid){
-
+        if(vm.userData.password == vm.userData.passCheck){
             // use the create function in the userService
             User.create(vm.userData)
                 .success(function(data) {
@@ -143,15 +116,14 @@ angular.module('userCtrl', ['userService', 'ui.bootstrap.showErrors', 'authServi
                         vm.invalidUserName = true;
                     }
                 });
-        //}
-        //else {
-        //     alert("There are invalid fields");
-        //}
+        }
+        else {
+             vm.noPassMatch = true;
+        }
             
     };  
 
 })
-    
 
 // ====
 // ----
